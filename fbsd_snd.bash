b1b5ad87f3bbf31952efe0baea3b5618acecb908
ccfbsd <cc@freebsd.org>
2025-04-17 14:17:29 -0400

    Add input argument <seconds>.
    Adjust these commands using this new <seconds> argument.
    
    Also simplify the usages of some commands.

--- fbsd_snd.bash
+++ fbsd_snd.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [  $# -ne 3 ]; then
-    echo -e "\nUsage:\n$0 <name> <src> <dst>\n"
-    echo -e "example: bash $0 cubic s1 r1\n"
+if [  $# -ne 4 ]; then
+    echo -e "\nUsage:\n$0 <name> <src> <dst> <seconds>\n"
+    echo -e "example: bash $0 cubic s1 r1 10\n"
     exit 1
 fi
 
 name=$1              # TCP congestion control name
 src=$2
 dst=$3
+seconds=$4
 
 SIFTR2_NAME="siftr2.ko"  # Module name
 SIFTR2_PATH="/root/siftr2"	# Module location
@@ -59,9 +60,11 @@ sysctl net.inet.siftr2.logfile=/var/log/${siftr_name} | tee -a ${log_name}
 netstat -sz > /dev/null 2>&1
 sysctl net.inet.siftr2.enabled=1 | tee -a ${log_name}
 
-iperf3 -B ${src} --cport ${tcp_port} -c ${dst} -p 5201 -l 1M -t 10 -i 1 -f m -VC ${name} > ${iperf_log_name}
+iperf3 -B ${src} --cport ${tcp_port} -c ${dst} -p 5201 -l 1M -t ${seconds} -i 1 -f m -VC ${name} > ${iperf_log_name}
 sysctl net.inet.siftr2.enabled=0 | tee -a ${log_name}
 netstat -sp tcp > ${netstat_file_name}
+
+sleep 1
 
 grep -E -A 2 "Summary Results" ${iperf_log_name} | grep "sender" | awk '{printf "%.2f\n", $7}' > ${snd_avg_goodput}
 
@@ -70,8 +73,14 @@ sed '1d' ${tmp_name} | sed '$d' | sed '$d' > ${throughput_timeline}
 
 # Run the binary and extract the flow_id value
 siftr2_log_abs_path=$(realpath /var/log/${siftr_name})
-flow_id=$(${log_review_tool} -f ${siftr2_log_abs_path} | awk -F'id:' '{print $2}' | awk '{print $1}')
-${log_review_tool} -f ${siftr2_log_abs_path} -s ${flow_id} >> ${log_name} 2>&1
+flow_id=$(${log_review_tool} -f "${siftr2_log_abs_path}" | awk -F'id:' '{print $2}' | awk '{print $1}' | tr -d '\r\n')
+${log_review_tool} -f "${siftr2_log_abs_path}" -p "${src}" -s "${flow_id}" >> "${log_name}" 2>&1
 
-cd /var/log/; ls -lh ${siftr_name} | tee -a ${log_name}; tar zcf ${siftr_name}.tgz ${siftr_name}
-rm ${siftr_name}; mv /var/log/${siftr_name}.tgz ${dir}/; rm ${tmp_name}
+plot_file=${src}.${flow_id}.txt
+echo "flow_id: [$flow_id]"
+echo "plot_file: [$plot_file]"
+
+ls -lh "${siftr2_log_abs_path}" | tee -a ${log_name}
+ls -lh "${plot_file}" | tee -a ${log_name}
+tar -zcf ${siftr_name}.tgz -C /var/log ${siftr_name}
+rm ${siftr2_log_abs_path}; rm ${tmp_name}
