337fb4a756b79abd3553a35518f66d932d49159e
ccfbsd <cc@freebsd.org>
2025-05-08 19:04:09 -0400

    add a second loop that runs tests with siftr2

--- fbsd_loop_test.bash
+++ fbsd_loop_test.bash
@@ -9,8 +9,21 @@ script=/root/research_scripts/fbsd_snd_nosiftr.bash
 
 for i in {1..3}; do
     folder="${name}.$i"
-    mkdir -p "$folder"
-    cd "$folder" || exit 1
+    mkdir -p "${folder}"
+    cd "${folder}" || exit 1
+
+    echo "Running ${script} in ${folder}..."
+    bash ${script} ${name} ${src} ${dst} ${seconds}
+
+    cd ..
+    echo ""
+done
+
+script=/root/research_scripts/fbsd_snd.bash
+for i in {1..3}; do
+    folder="${name}.siftr.$i"
+    mkdir -p "${folder}"
+    cd "${folder}" || exit 1
 
     echo "Running ${script} in $folder..."
     bash ${script} ${name} ${src} ${dst} ${seconds}
