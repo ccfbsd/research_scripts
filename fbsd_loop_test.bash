8569347d3b828a86c796bc3382c1ab7975256c81
ccfbsd <cc@freebsd.org>
2025-05-08 18:58:54 -0400

    add a new script that tests repeately in fbsd

---
+++ fbsd_loop_test.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+name=$1              # TCP congestion control name
+src=$2
+dst=$3
+seconds=$4
+
+script=/root/research_scripts/fbsd_snd_nosiftr.bash
+
+for i in {1..3}; do
+    folder="${name}.$i"
+    mkdir -p "$folder"
+    cd "$folder" || exit 1
+
+    echo "Running ${script} in $folder..."
+    bash ${script} ${name} ${src} ${dst} ${seconds}
+
+    cd ..
+    echo ""
+done
