ca5a30fdb96af6dbdcd09d2b0eea4a72441c5813
ccfbsd <cc@freebsd.org>
2025-05-12 10:37:34 -0400

    add input check and only run test with siftr2 once

--- fbsd_loop_test.bash
+++ fbsd_loop_test.bash
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+if [  $# -ne 4 ]; then
+    echo -e "\nUsage:\n$0 <name> <src> <dst> <seconds>\n example: bash $0 cubic s1 r1 10\n"
+    exit 1
+fi
+
 name=$1              # TCP congestion control name
 src=$2
 dst=$3
 seconds=$4
+
+SIFTR2_NAME="siftr2.ko"  # Module name
+SIFTR2_PATH="/root/siftr2"  # Module location
+
+# Check if the module is already loaded
+if ! kldstat | grep -q "$SIFTR2_NAME"; then
+    echo "Module $SIFTR2_NAME is not loaded. Loading it now..."
+    
+    # Try loading the module
+    if kldload ${SIFTR2_PATH}/${SIFTR2_NAME}; then
+        echo "Module $SIFTR2_NAME loaded successfully."
+    else
+        echo "Failed to load module $SIFTR2_NAME. Check if the module exists or requires dependencies."
+        exit 1
+    fi
+else
+    echo "Module $SIFTR2_NAME is already loaded."
+fi
 
 script=/root/research_scripts/fbsd_snd_nosiftr.bash
 
@@ -16,11 +39,11 @@ for i in {1..3}; do
     bash ${script} ${name} ${src} ${dst} ${seconds}
 
     cd ..
-    echo ""
+    echo "" && sleep 10
 done
 
 script=/root/research_scripts/fbsd_snd.bash
-for i in {1..3}; do
+for i in {1..1}; do
     folder="${name}.siftr.$i"
     mkdir -p "${folder}"
     cd "${folder}" || exit 1
