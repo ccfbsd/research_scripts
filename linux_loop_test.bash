12c8e85a4c1315884c1daa3f2c2e6a97e080e4fb
ccfbsd <cc@freebsd.org>
2025-05-30 08:38:24 -0400

    always get a trace file in each iteration

--- linux_loop_test.bash
+++ linux_loop_test.bash
@@ -10,30 +10,31 @@ src=$2
 dst=$3
 seconds=$4
 
-script=/root/research_scripts/linux_snd_notrace.bash
-
-for i in {1..4}; do
-    folder="${name}.$i"
-    mkdir -p "${folder}"
-    cd "${folder}" || exit 1
-
-    echo "Running ${script} in ${folder}..."
-    bash ${script} ${name} ${src} ${dst} ${seconds}
-
-    cd ..
-    echo ""
-    sleep 10
-done
+interval=$(echo "${seconds} * 1.1" | bc | awk '{printf "%d\n", $1}')
+start_time=$(date +%s)
+next_time=$(( start_time + interval ))
+echo "start_time: [$start_time], interval: [$interval]"
 
 script=/root/research_scripts/linux_snd.bash
-for i in {1..1}; do
+for i in {1..5}; do
     folder="${name}.trace.$i"
     mkdir -p "${folder}"
     cd "${folder}" || exit 1
 
-    echo "Running ${script} in $folder..."
+    echo "[$(date +%s)] Running ${script} in $folder..."
     bash ${script} ${name} ${src} ${dst} ${seconds}
-
+    finish_time=$(date +%s)
+    echo "script running finished at: [${finish_time}]"
     cd ..
-    echo ""
+    echo -e "next run is scheduled at: [${next_time}], delta: [$(( next_time - finish_time ))]\n"
+
+    while true; do
+        now=$(date +%s)
+        if [ "$now" -lt "$next_time" ]; then
+            sleep 0.1
+        else
+            next_time=$(( next_time + interval ))
+            break
+        fi
+    done
 done
