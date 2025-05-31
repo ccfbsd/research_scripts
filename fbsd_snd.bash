b1a001f93e75f90d754e529133b3d79563ba7e9c
ccfbsd <cc@freebsd.org>
2025-05-31 08:20:00 -0400

    use iperf, instead of iperf3, and also cleanup

--- fbsd_snd.bash
+++ fbsd_snd.bash
@@ -10,6 +10,8 @@ src=$2
 dst=$3
 seconds=$4
 
+start_time=$(date +%s.%N)
+
 SIFTR2_NAME="siftr2.ko"  # Module name
 SIFTR2_PATH="/root/siftr2"	# Module location
 
@@ -38,14 +40,14 @@ fi
 
 dir=$(pwd)
 tcp_port=54321
-log_name="${dir}/${src}.test.log"
+iperf_svr_port=5001
+log_name="${src}.test.log"
 siftr_name="${src}.${name}.siftr2"
 siftr2_log_real_path="${dir}/${siftr_name}"
-netstat_file_name="${dir}/${src}.${name}.netstat"
-iperf_log_name="${dir}/${src}.iperf3_output.log"
-throughput_timeline="${dir}/${src}.mbps_timeline.txt"
-snd_avg_goodput="${dir}/${src}.avg.goodput"
-tmp_name="${dir}/${src}.tmp.log"
+netstat_file_name="${src}.${name}.netstat"
+iperf_log_name="${src}.iperf_output.log"
+throughput_timeline="${src}.mbps_timeline.txt"
+snd_avg_goodput="${src}.avg.goodput"
 
 uname -v | tee ${log_name}
 sysctl net.inet.tcp.functions_default | tee -a ${log_name}
@@ -54,7 +56,7 @@ sysctl net.inet.tcp.hostcache.enable=0 | tee -a ${log_name}
 # force to close the control socket after 4 seconds
 sysctl net.inet.tcp.msl=2000 | tee -a ${log_name}
 sysctl net.inet.tcp.cc.algorithm=${name} | tee -a ${log_name}
-sysctl net.inet.siftr2.port_filter=${tcp_port} | tee -a ${log_name}
+sysctl net.inet.siftr2.port_filter=${iperf_svr_port} | tee -a ${log_name}
 sysctl net.inet.siftr2.cwnd_filter=1 | tee -a ${log_name}
 sysctl net.inet.siftr2.ppl=1 | tee -a ${log_name}
 sysctl net.inet.siftr2.logfile=${siftr2_log_real_path} | tee -a ${log_name}
@@ -62,49 +64,43 @@ kldstat | tee -a ${log_name}
 netstat -sz > /dev/null 2>&1
 sysctl net.inet.siftr2.enabled=1 | tee -a ${log_name}
 
-iperf3 -B ${src} --cport ${tcp_port} -c ${dst} -p 5201 -l 1M -t ${seconds} -i 1 -f m -VC ${name} > ${iperf_log_name}
+#iperf3 -B ${src} --cport ${tcp_port} -c ${dst} -p 5201 -l 1M -t ${seconds} -i 1 -f m -VC ${name} > ${iperf_log_name}
+iperf -B ${src} -c ${dst} -t ${seconds} -i 1 -f m -Z ${name} > ${iperf_log_name}
 sysctl net.inet.siftr2.enabled=0 | tee -a ${log_name}
 netstat -sp tcp > ${netstat_file_name}
 
-sleep 1
+awk '/sec/ {split($3, interval, "-"); printf "%d\t%s\n", int(interval[2]), $7}'\
+    ${iperf_log_name} | sed '$d' > ${throughput_timeline}
+tail -n 1 ${iperf_log_name} | awk '{printf "%.1f\n", $7}' > ${snd_avg_goodput}
 
-grep -E -A 2 "Summary Results" ${iperf_log_name} | grep "sender" | awk '{printf "%.2f\n", $7}' > ${snd_avg_goodput}
 avg_goodput=$(cat "${snd_avg_goodput}" | tr -d '\r\n')
 echo "sender average throughput: [${avg_goodput}]"
 
-awk '/sec/ {split($3, interval, "-"); printf "%d\t%s\n", int(interval[2]), $7}' ${iperf_log_name} > ${tmp_name}
-sed '1d' ${tmp_name} | sed '$d' | sed '$d' > ${throughput_timeline}
-
 # Run the binary and extract the flow_id value
-
 flow_id=$(${log_review_tool} -f "${siftr2_log_real_path}" | awk -F'id:' '{print $2}' | awk '{print $1}' | tr -d '\r\n')
 ${log_review_tool} -f "${siftr2_log_real_path}" -p "${src}" -s "${flow_id}" >> "${log_name}" 2>&1
+plot_file="${src}.${flow_id}.txt"
 
 cwnd_stats=$(grep "avg_cwnd:" "${log_name}" | awk '{$1=$1; print}')
 cwnd_stats="${cwnd_stats//_/\\\\\\_}"
-#echo "[${cwnd_stats}]"
 srtt_stats=$(grep "avg_srtt:" "${log_name}" | awk '{$1=$1; print}')
 srtt_stats="${srtt_stats//_/\\\\\\_}"
-#echo "[${srtt_stats}]"
 
 max_cwnd=$(grep "max_cwnd" "${log_name}" | awk '{printf "%d\n", $6}')
 ymax_cwnd=$(echo "$max_cwnd * 1.25" | bc)
 max_srtt=$(grep "max_srtt" "${log_name}" | awk '{printf "%d\n", $6}')
 ymax_srtt=$(echo "$max_srtt * 1.25" | bc)
-#echo "[${max_cwnd}][${ymax_cwnd}]"
-#echo "[${max_srtt}][${ymax_srtt}]"
 
-plot_file=${src}.${flow_id}.txt
-
-du -hd0 "${siftr2_log_real_path}" | tee -a ${log_name}
-du -hd0 "${plot_file}" | tee -a ${log_name}
+du -hd0 ${siftr2_log_real_path} | tee -a ${log_name}
+du -hd0 ${plot_file} | tee -a ${log_name}
 tar -zcf ${siftr_name}.tgz -C ${dir} ${siftr_name}
-rm -r ${siftr2_log_real_path} ${tmp_name}
+rm -r ${siftr2_log_real_path}
 
 echo "generating gnuplot figure..."
 
 cwnd_title_str="${src} ${name} cwnd chart"
 srtt_title_str="${src} ${name} srtt chart"
+pt_interval=$((seconds * 100))
 
 gnuplot -persist << EOF
 set encoding utf8
@@ -113,40 +109,35 @@ set output "${src}.cwnd_srtt.${flow_id}.pdf"
 set multiplot layout 2,1 title "Flow Analysis" offset 4.0,0.0
 
 # linecolor(lc), linetype(lt), linewidth(lw), dashtype(dt), pointtype(pt)
-set style line 1 lc rgb 'red' lt 1 lw 2 pt 1 pointsize 1 pointinterval 1000
+set style line 1 lc rgb 'red' lt 1 lw 2 pt 1 pointsize 1 pointinterval ${pt_interval}
 
-# First plot cwnd
-set title "${cwnd_title_str}"
-set xlabel "time (second)"
-set ylabel "cwnd (byte)"
 set xtics nomirror
 set ytics nomirror
-set tmargin 3		# Top margin
+set tmargin 3       # Top margin
 set mxtics
-set xrange [0:${seconds}]
-set yrange [0:${ymax_cwnd}]
 set autoscale fix
 set key box opaque vertical right top reverse Left samplen 2 width 1 spacing 1.5
 set boxwidth 2 relative
+set xlabel "time (second)"
+set xrange [0:${seconds}]
 
+# First plot cwnd
+set title "${cwnd_title_str}"
+set ylabel "cwnd (byte)"
+set yrange [0:${ymax_cwnd}]
 plot "${plot_file}" using 2:3 title "flow1: ${cwnd_stats}" with linespoints ls 1
 
 # Second plot
 set title "${srtt_title_str}"
-set xlabel "time (second)"
 set ylabel "srtt (microsecond)"
-set xtics nomirror
-set ytics nomirror
-set tmargin 3		# Top margin
-set mxtics
-set xrange [0:${seconds}]
 set yrange [0:${ymax_srtt}]
-set autoscale fix
-set key box opaque vertical right top reverse Left samplen 2 width 1 spacing 1.5
-set boxwidth 2 relative
-
 plot "${plot_file}" using 2:5 title "flow1: ${srtt_stats}" with linespoints ls 1
 
 unset multiplot
 unset output
 EOF
+
+end_time=$(date +%s.%N)
+elapsed=$(echo "$end_time - $start_time" | bc)
+
+printf "Execution time: %.1f seconds\n" "$elapsed"
