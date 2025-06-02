65d063f98e44a9a35829c3a549d8ab02a3c5fec2
ccfbsd <cc@freebsd.org>
2025-06-02 17:30:36 -0400

    use an input argument for xrange

--- gnuplot_cwnd.bash
+++ gnuplot_cwnd.bash
@@ -1,11 +1,16 @@
 #! /bin/bash
 
-# if not 2 arguments supplied, display usage
-if [  $# -ne 2 ]
+# if not 3 arguments supplied, display usage
+if [  $# -ne 3 ]
 then
-	echo -e "requires 2 input arguments\nUsage: $0 n1fbsd.plot.txt n2fbsd.plot.txt\n"
+	echo -e "requires 3 input arguments\nUsage: $0 n1fbsd.plot.txt n2fbsd.plot.txt 300\n"
 	exit 1
 fi
+
+data1=$1
+data2=$2
+seconds=$3
+pt_interval=$((seconds * 100))
 
 echo "generating gnuplot figure..."
 gnuplot -persist << EOF
@@ -19,19 +24,19 @@ set xtics nomirror
 set ytics nomirror
 set tmargin 3		# Top margin
 set mxtics
-set xrange [0:3e2]
+set xrange [0:${seconds}]
 set yrange [0:6e6]
 set autoscale fix
 
 # linecolor(lc), linetype(lt), linewidth(lw), dashtype(dt), pointtype(pt)
-set style line 1 lc rgb 'red' lt 1 lw 2 pt 1 pointsize 1 pointinterval 10000
-set style line 2 lc rgb 'blue' lt 1 lw 2 pt 2 pointsize 1 pointinterval 10000
-set style line 3 lc rgb 'green' lt 1 lw 2 pt 3 pointsize 1 pointinterval 10000
+set style line 1 lc rgb 'red' lt 1 lw 2 pt 1 pointsize 1 pointinterval ${pt_interval}
+set style line 2 lc rgb 'blue' lt 1 lw 2 pt 2 pointsize 1 pointinterval ${pt_interval}
+set style line 3 lc rgb 'green' lt 1 lw 2 pt 3 pointsize 1 pointinterval ${pt_interval}
 
 set key box opaque vertical right top reverse Left samplen 2 width 1 spacing 1.5
 set boxwidth 2 relative
 
-plot "$1" using 1:3 title "flow1" with linespoints ls 1, \
-     "$2" using 1:3 title "flow2" with linespoints ls 2
+plot "${data1}" using 1:3 title "flow1" with linespoints ls 1, \
+     "${data2}" using 1:3 title "flow2" with linespoints ls 2
 
 EOF
