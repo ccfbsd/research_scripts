490e3b08c5822b22b0befbe8a8bd7838d9e5f5fb
ccfbsd <cc@freebsd.org>
2025-06-23 11:54:39 -0400

    add ploting sender throughpt chart

--- linux_snd.bash
+++ linux_snd.bash
@@ -104,20 +104,22 @@ done < ${log_name}
 total_socks=$(grep -oP 'flow_count:\s*\K[0-9]+' ${log_name})
 ymax_cwnd=$(echo "${max_cwnd_global} + ${max_cwnd_global} * 0.2 * ${parallel}" | bc)
 ymax_srtt=$(echo "${max_srtt_global} + ${max_srtt_global} * 0.2 * ${parallel}" | bc)
+ymax_thruput=$(echo "${avg_goodput} * 1.5" | bc)
 #echo "total_socks: [${total_socks}], ymax_srtt: [${ymax_srtt}], ymax_cwnd: [${ymax_cwnd}]"
 
 echo "generating gnuplot figure..."
 
 cwnd_title_str="sender '${src}' ${name} cwnd chart"
 srtt_title_str="sender '${src}' ${name} srtt chart"
+throughput_title_str="sender '${src}' ${name} throughput chart"
 pt_interval=$((seconds * 100))
 
 gnuplot -persist << EOF
 set encoding utf8
 set term pdfcairo color lw 1 dashlength 1 enhanced font "DejaVu Sans Mono,16"\
     dashed size 12in,9in background rgb "white"
-set output "${src}.cwnd_srtt.pdf"
-set multiplot layout 2,1 title "TCP Analysis" offset 4.0,0.0
+set output "${src}.cwnd_srtt_thruput.pdf"
+set multiplot layout 3,1 title "TCP Analysis" offset 4.0,0.0
 
 # linecolor(lc), linetype(lt), linewidth(lw), dashtype(dt), pointtype(pt)
 set style line 1 lc rgb 'red' lt 1 lw 2 pt 1 pointsize 1 pointinterval ${pt_interval}
@@ -156,7 +158,7 @@ $(
     done
 )
 
-# Second plot
+# Second plot srtt
 set title "${srtt_title_str}"
 set ylabel "srtt (microsecond)"
 set yrange [0:${ymax_srtt}]
@@ -176,6 +178,15 @@ $(
         fi
     done
 )
+
+# Third plot throughput
+set title "${throughput_title_str}"
+set ylabel "throughput (Mbits/sec)"
+set yrange [0:${ymax_thruput}]
+# linecolor(lc), linetype(lt), linewidth(lw), dashtype(dt), pointtype(pt)
+set style line 1 lc rgb 'red' lt 1 lw 2 pt 1 pointsize 1 pointinterval 1
+plot "${throughput_timeline}" using 1:2  with linespoints ls 1 title \
+     sprintf("${src}: average throughput = %.1f Mbits/sec", ${avg_goodput})
 
 unset multiplot
 unset output
