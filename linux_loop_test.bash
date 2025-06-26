882e555f7a302f176ecd2677d954eff71277264a
ccfbsd <cc@freebsd.org>
2025-06-26 11:55:59 -0400

    use the updated linux_snd.bash

--- linux_loop_test.bash
+++ linux_loop_test.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [  $# -ne 4 ]; then
-    echo -e "\nUsage:\n$0 <name> <src> <dst> <seconds>\n example: bash $0 cubic s1 r1 10\n"
+if [  $# -ne 5 ]; then
+    echo -e "\nUsage:\n$0 <name> <src> <dst> <seconds> <num_of_streams> \n"
+    echo -e " example: bash $0 cubic s1 r1 10 2\n"
     exit 1
 fi
 
@@ -9,44 +10,21 @@ name=$1              # TCP congestion control name
 src=$2
 dst=$3
 seconds=$4
+parallel=$5         # number of parallel client streams to run
 
-interval=$(echo "${seconds} + 10" | bc | awk '{printf "%d\n", $1}')
+interval=$(echo "${seconds} * 1.2" | bc | awk '{printf "%d\n", $1}')
 start_time=$(date +%s)
 next_time=$(( start_time + interval ))
 echo "start_time: [$start_time], interval: [$interval]"
 
-script=/root/research_scripts/linux_snd_notrace.bash
-for i in {1..5}; do
+script=/root/research_scripts/linux_snd.bash
+for i in {1..6}; do
     folder="${name}.$i"
     mkdir -p ${folder}
     cd ${folder} || exit 1
 
     echo "[$(date +%s)] Running ${script} in ${folder}..."
-    bash ${script} ${name} ${src} ${dst} ${seconds}
-    finish_time=$(date +%s)
-    echo "script running finished at: [${finish_time}]"
-    cd ..
-    echo -e "next run is scheduled at: [${next_time}], delta: [$(( next_time - finish_time ))]\n"
-
-    while true; do
-        now=$(date +%s)
-        if [ "$now" -lt "$next_time" ]; then
-            sleep 0.1
-        else
-            next_time=$(( next_time + interval ))
-            break
-        fi
-    done
-done
-
-script=/root/research_scripts/linux_snd.bash
-for i in {6..6}; do
-    folder="${name}.trace.$i"
-    mkdir -p ${folder}
-    cd ${folder} || exit 1
-
-    echo "[$(date +%s)] Running ${script} in ${folder}..."
-    bash ${script} ${name} ${src} ${dst} ${seconds}
+    bash ${script} ${name} ${src} ${dst} ${seconds} ${parallel}
     finish_time=$(date +%s)
     echo "script running finished at: [${finish_time}]"
     cd ..
